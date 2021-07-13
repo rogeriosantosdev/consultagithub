1ad1acbe54e10af4b2172c4f6eefdf4d7e35e003
Rogério <rogeriosantosdev@gmail.com>
2021-07-12 22:30:20 -0300

    use History

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './styled';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function Repositories (){
-    const [repositories, setRepositories] = useState([])
-    useEffect(() => {
-        let repositoriesName = localStorage.getItem('repositoriesName')
-        repositoriesName = JSON.parse(repositoriesName);
-        setRepositories(repositoriesName)
-        //localStorage.clear();
-    }, [])
+    const history = useHistory();
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() =>{
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName !== null){
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/');
+        }
+    },[])
+
     return (
         <S.Container>
             <S.Title>Repositórios</S.Title>
